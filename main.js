9f1b7c1256f68f6092132d66bb984268c0c176e4
fed1k <allamurotovcoder@mail.ru>
2022-02-28 22:39:09 +0500

    Add functionality to close-popup-menu

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-const mainMenu = document.querySelector('#main-menu')
-const popUpMenu = document.querySelector('#popup-menu')
-const toggler = document.querySelector('.sandwich')
-const header = document.querySelector('header')
+const mainMenu = document.querySelector('#main-menu');
+const popUpMenu = document.querySelector('#popup-menu');
+const toggler = document.querySelector('.sandwich');
+const header = document.querySelector('header');
+const close = document.querySelector('.close-icon');
 
-const clickToggler = (e) => {
-  header.style.cssText = 'height: 100vh; background-color: #3c3a39;'
-  mainMenu.style.display = 'none'
-  mainMenu.style.position = 'static'
-  popUpMenu.style.display = 'block'
-}
+const clickToggler = () => {
+  header.style.cssText = 'height: 100vh; background-color: #3c3a39;';
+  mainMenu.style.display = 'none';
+  mainMenu.style.position = 'static';
+  popUpMenu.style.display = 'block';
+};
 
-toggler.addEventListener('click', clickToggler)
+toggler.addEventListener('click', clickToggler);
+
+close.addEventListener('click', () => {
+  header.style.cssText = 'height: 72px; background-color: #3c3a39;';
+  mainMenu.style.display = 'flex';
+  popUpMenu.style.display = 'none';
+});
