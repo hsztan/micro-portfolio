425c9d5ae7961e0b8696ecf7fe7e3c8c6188471d
Henry Nawrocki <hdev@sztanski.com>
2022-02-28 15:55:34 -0500

    Create data.js file and add data for recent works in objects

---
+++ data.js
@@ -0,0 +1,60 @@
+const works = [
+  {
+    name: 'Multi-Post Stories',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    featuredImage: './assets/images/featured-placeholder.png',
+    technologies: ['css', 'html', 'bootstrap', 'Ruby'],
+    urlLive: '',
+    urlRepo: '',
+  },
+  {
+    name: 'Professional Art Printing Data More',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: './assets/images/card-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    urlLive: '',
+    urlRepo: '',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: './assets/images/card-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    urlLive: '',
+    urlRepo: '',
+  },
+  {
+    name: 'Website Portfolio',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    ffeaturedImage: './assets/images/card-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    urlLive: '',
+    urlRepo: '',
+  },
+  {
+    name: 'Professional Art Printing Data More',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: './assets/images/card-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    urlLive: '',
+    urlRepo: '',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featuredImage: './assets/images/card-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    urlLive: '',
+    urlRepo: '',
+  },
+  {
+    name: 'Website Portfolio',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    ffeaturedImage: './assets/images/card-placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    urlLive: '',
+    urlRepo: '',
+  },
+];
+
+export default works;
