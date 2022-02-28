24b9d9d8a77e2979756236884b5df39132373f01
Henry Nawrocki <hdev@sztanski.com>
2022-02-28 11:28:00 -0500

    Change header height to 100vh with dom

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const mainMenu = document.querySelector('#main-menu')
 const popUpMenu = document.querySelector('#popup-menu')
 const sandwich = document.querySelector('.sandwich')
+const header = document.querySelector('header')
 
 const clickSandwich = (e) => {
+  header.style.height = '100vh'
   mainMenu.style.display = 'none'
-  mainMenu.style.opacity = 0
+  mainMenu.style.position = 'static'
   popUpMenu.style.display = 'block'
 }
 
