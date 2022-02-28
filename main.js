99a621df6b5c44a1a073bcf76e872944bcd70b33
Henry Nawrocki <hdev@sztanski.com>
2022-02-28 12:02:00 -0500

    Change popup-menu css selector to correct div

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const mainMenu = document.querySelector('#main-menu');
-const popUpMenu = document.querySelector('#popup-menu');
-const toggler = document.querySelector('.sandwich');
-const header = document.querySelector('header');
+const mainMenu = document.querySelector('#main-menu')
+const popUpMenu = document.querySelector('#popup-menu')
+const toggler = document.querySelector('.sandwich')
+const header = document.querySelector('header')
 
 const clickToggler = (e) => {
-  header.style.cssText = 'height: 100vh; background-color: #3c3a39;';
-  mainMenu.style.display = 'none';
-  mainMenu.style.position = 'static';
-  popUpMenu.style.display = 'block';
-};
+  header.style.cssText = 'height: 100vh; background-color: #3c3a39;'
+  mainMenu.style.display = 'none'
+  mainMenu.style.position = 'static'
+  popUpMenu.style.display = 'block'
+}
 
-toggler.addEventListener('click', clickToggler);
+toggler.addEventListener('click', clickToggler)
