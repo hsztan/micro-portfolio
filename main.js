bc456e062dfe0c0cc0b3f992e4d2416c7102fa54
fed1k <allamurotovcoder@mail.ru>
2022-03-01 02:37:48 +0500

    Add main selector for dom creation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import works from './data.js';
+
 const mainMenu = document.querySelector('#main-menu');
 const popUpMenu = document.querySelector('#popup-menu');
 const hamburger = document.querySelector('.sandwich');
@@ -26,3 +28,61 @@ close.addEventListener('click', resetMenu);
 menuItem.forEach((item) => {
   item.addEventListener('click', resetMenu);
 });
+
+// Mobile-menu ends here
+
+// Getting reference
+const btn = document.querySelectorAll('.btn');
+const featuredWork = document.querySelector('#featured-work');
+const cardsWrapper = document.querySelector('.cards-wrapper');
+
+featuredWork.innerHTML = `
+        <div class="title">
+          <h2>My Recent Works</h2>
+          <div class="separator"></div>
+        </div>
+        <div class="featured">
+          <div class="image">
+            <img
+              src="./assets/images/featured-placeholder.png"
+              alt="featured work"
+            />
+          </div>
+          <div class="content">
+            <h3>Multi-Post Stories</h3>
+            <p>
+              A daily selection of privately personalized reads; no accounts or
+              sign-ups required. has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a standard
+              dummy text.
+            </p>
+            <ul class="tags">
+              <li><a href="#">css</a></li>
+              <li><a href="#">html</a></li>
+              <li><a href="#">bootstrap</a></li>
+              <li><a href="#">Ruby</a></li>
+            </ul>
+            <button class="btn btn-main" type="button">See Project</button>
+          </div>
+        </div>
+`;
+for (let i = 1; i < works.length; i += 1) {
+
+}
+cardsWrapper.innerHTML = `
+          <div class="card">
+            <div class="content">
+              <h4 class="title">Profesional Art Printing Data More</h4>
+              <p class="summary">
+                A daily selection of privately personalized reads; no accounts
+                or sign-ups required. has been the industry's standard
+              </p>
+              <ul class="tags">
+                <li><a href="#">html</a></li>
+                <li><a href="#">bootstrap</a></li>
+                <li><a href="#">Ruby</a></li>
+              </ul>
+            </div>
+            <button class="btn btn-main" type="button">See Project</button>
+          </div>
+`;
