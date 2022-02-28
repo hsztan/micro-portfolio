392b79d648e98ef1c5209806dffabd24791459b8
fed1k <allamurotovcoder@mail.ru>
2022-02-28 22:49:34 +0500

    Complete functionality to popup-menu

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const mainMenu = document.querySelector('#main-menu');
 const popUpMenu = document.querySelector('#popup-menu');
-const toggler = document.querySelector('.sandwich');
+const hamburger = document.querySelector('.sandwich');
+const menuItem = document.querySelectorAll('.popup-list-item');
 const header = document.querySelector('header');
 const close = document.querySelector('.close-icon');
 
-const clickToggler = () => {
+const clickHamburger = () => {
   header.style.cssText = 'height: 100vh; background-color: #3c3a39;';
   mainMenu.style.display = 'none';
   mainMenu.style.position = 'static';
   popUpMenu.style.display = 'block';
 };
 
-toggler.addEventListener('click', clickToggler);
+hamburger.addEventListener('click', clickHamburger);
 
-close.addEventListener('click', () => {
+function resetMenu() {
   header.style.cssText = 'height: 72px; background-color: #3c3a39;';
   mainMenu.style.display = 'flex';
   popUpMenu.style.display = 'none';
+}
+close.addEventListener('click', resetMenu);
+
+menuItem.forEach((item) => {
+  item.addEventListener('click', resetMenu);
 });
