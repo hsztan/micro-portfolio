3370b58dafee025b4346d36661721f11b8fa8c80
Henry Nawrocki <hdev@sztanski.com>
2022-03-31 14:29:49 -0500

    Add Fiacs project

--- data.js
+++ data.js
@@ -1,23 +1,26 @@
 const works = [
   {
     name: 'Multi-Post Stories',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
     featuredImage: './assets/images/featured-placeholder.png',
     technologies: ['css', 'html', 'bootstrap', 'Ruby'],
     urlLive: '',
     urlRepo: '',
   },
   {
-    name: 'Professional Art Printing Data More',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    featuredImage: './assets/images/modal-work-thumbnail.png',
-    technologies: ['html', 'bootstrap', 'Ruby'],
-    urlLive: '',
-    urlRepo: '',
+    name: 'FIACS Arrival Conference',
+    description:
+      'A Conference Website made with Vanilla Javascript, HTML and CSS',
+    featuredImage: './assets/images/fiacs-project.png',
+    technologies: ['html', 'css', 'Javacsipt'],
+    urlLive: 'https://hsztan.github.io/the-conference/',
+    urlRepo: 'https://github.com/hsztan/the-conference',
   },
   {
     name: 'Data Dashboard Healthcare',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './assets/images/modal-work-thumbnail.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     urlLive: '',
@@ -25,7 +28,8 @@ const works = [
   },
   {
     name: 'Website Portfolio',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './assets/images/modal-work-thumbnail.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     urlLive: '',
@@ -33,7 +37,8 @@ const works = [
   },
   {
     name: 'Professional Art Printing Data More',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './assets/images/modal-work-thumbnail.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     urlLive: '',
@@ -41,7 +46,8 @@ const works = [
   },
   {
     name: 'Data Dashboard Healthcare',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './assets/images/modal-work-thumbnail.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     urlLive: '',
@@ -49,7 +55,8 @@ const works = [
   },
   {
     name: 'Website Portfolio',
-    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
     featuredImage: './assets/images/modal-work-thumbnail.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     urlLive: '',
